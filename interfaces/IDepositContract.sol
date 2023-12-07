a9286d1334b04a642df5701026a2325149ebf24b
polarzero <0xpolarzero@gmail.com>
2023-12-07 14:07:32 +0100

    Add IDepositContract interface

---
+++ interfaces/IDepositContract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+interface DepositContract {
+    event DepositEvent(bytes pubkey, bytes withdrawal_credentials, bytes amount, bytes signature, bytes index);
+
+    function deposit(
+        bytes memory pubkey,
+        bytes memory withdrawal_credentials,
+        bytes memory signature,
+        bytes32 deposit_data_root
+    ) external payable;
+    function get_deposit_count() external view returns (bytes memory);
+    function get_deposit_root() external view returns (bytes32);
+    function supportsInterface(bytes4 interfaceId) external pure returns (bool);
+}
