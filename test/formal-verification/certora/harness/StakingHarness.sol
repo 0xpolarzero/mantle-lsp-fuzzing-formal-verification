f21d80fa0dacc0857a540be09302a91d6d504892
polarzero <0xpolarzero@gmail.com>
2023-12-11 17:17:22 +0100

    Init Certora

--- test/formal-verification/certora/harness/StakingHarness.sol
+++ test/formal-verification/certora/harness/StakingHarness.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 
 import "src/Staking.sol";
 
-contract VoteLockupHarness is Staking {
-    constructor() Staking() {}
+contract StakingHarness is Staking {
+    constructor() Staking() {
+        // call init
+    }
 }
