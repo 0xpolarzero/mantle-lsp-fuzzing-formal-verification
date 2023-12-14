88aab33f4cc8a9a57052b37c77cb8b924ac7a467
polarzero <0xpolarzero@gmail.com>
2023-12-14 08:59:28 +0100

    Init Certora tests

---
+++ test/formal-verification/certora/README.md
@@ -0,0 +1,3 @@
+Here we don't really care about the proxy, and we interact directly with a fresh Staking contract.
+
+We don't give arguments and instances for all contracts in the configuration file, because we initialize the staking contract in the harness constructor anyway.
--- test/formal-verification/certora/harness/StakingHarness.sol
+++ test/formal-verification/certora/harness/StakingHarness.sol
@@ -2,10 +2,18 @@
 
 pragma solidity ^0.8.20;
 
-import "src/Staking.sol";
+/// @dev Copy of Staking
+import "test/formal-verification/certora/src/Staking.sol";
 
 contract StakingHarness is Staking {
+    address public admin;
+    address public manager;
+    address public allocatorService;
+    address public initiatorService;
+
     constructor() Staking() {
         // call init
+        // or since we already define the mocks we don't need init, just associate addresses to the ones above
+        // or maybe yes to initiate the roles
     }
 }
